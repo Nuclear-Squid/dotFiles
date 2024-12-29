cdbcf756478ce1f19b4dc83cb198e415084c96fc
Nuclear Squid <leo@cazenave.cc>
2024-12-30 00:41:02 +0100

    Much better "nothing" alias.

--- shell_aliases.sh
+++ shell_aliases.sh
@@ -1,31 +1,66 @@
-alias l="eza -l"
-alias la="eza -la"
+eza_extra_options=(--group-directories-first --git --icons --no-quotes)
+
+alias l="eza -l $eza_extra_options"
+alias la="eza -la $eza_extra_options"
+alias rm="rm -I"
 alias cat="bat"
 alias chux="chmod u+x"
 alias ggez="sudo nixos-rebuild switch --show-trace"
+alias vi="neovide --fork --"
 
 default_ls_command() {
-    eza -l --git-ignore
+    eza -l --git-ignore $eza_extra_options $@
+}
+
+get_git_repo_path() {
+    echo "$(git rev-parse --show-toplevel 2>/dev/null)"
 }
 
 magic-enter() {
     if [ -z $BUFFER ]; then
         echo
-        default_ls_command
+        if [ $(get_git_repo_path) != "" ]; then
+            paste <(default_ls_command --color=always) <(git -c color.ui=always st | sed 's/\t/        /g') | column -s $'\t' -t
+        else
+            default_ls_command
+        fi
         echo -ne "\x1b[A"
     fi
-    zle accept-line
+    zle .accept-line
 }
 
-zle -N magic-enter
-bindkey "^M" magic-enter
+# Fixes problems with zsh-autocompletion. I have *absolutely no idea*
+# how **any** of this works
+# https://github.com/zsh-users/zsh-autosuggestions/issues/525#issuecomment-932393117
+
+# Wrapper for the accept-line zle widget (run when pressing Enter)
+
+# If the wrapper already exists don't redefine it
+(( ! ${+functions[_magic-enter_accept-line]} )) || return 0
+
+case "$widgets[accept-line]" in
+  # Override the current accept-line widget, calling the old one
+  user:*) zle -N _magic-enter_orig_accept-line "${widgets[accept-line]#user:}"
+    function _magic-enter_accept-line() {
+      magic-enter
+      zle _magic-enter_orig_accept-line -- "$@"
+    } ;;
+  # If no user widget defined, call the original accept-line widget
+  builtin) function _magic-enter_accept-line() {
+      magic-enter
+      zle .accept-line
+    } ;;
+esac
+
+zle -N accept-line _magic-enter_accept-line
+
 
 
 cf() { mkdir -p $1 && builtin cd $1 }
 
 # cd, but better (what exactly do you mean by "just use `ls` you lazy fuck"?)
 cd() {
-    local previous_git_repo="$(git rev-parse --show-toplevel 2>/dev/null)"
+    local previous_git_repo=$(get_git_repo_path)
     local auto_jumped_path=""
     builtin cd "${1:-$HOME}" || return
     # Only one thing in current dir and it’s another dir
