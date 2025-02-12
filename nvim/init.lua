39e345de83fd684b4785777ad9ba8027c2710e41
Nuclear Squid <leo@cazenave.cc>
2025-02-12 14:43:45 +0100

    Nvim: new autocmds

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,12 +12,46 @@ local default_timer = 350
 --  ───────────────────────────< Autocommands >────────────────────────
 autocmd('InsertLeave', { pattern = '*', command = 'write' })
 
+-- Disable folding in Telescope's result window.
+autocmd("FileType", { pattern = "TelescopeResults", command = [[setlocal nofoldenable]] })
+
 autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank { timeout = default_timer }
   end,
+})
+
+
+autocmd("BufReadPost", {
+    desc = 'Restore cursor to file position in previous editing session',
+    callback = function(args)
+        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+        local line_count = vim.api.nvim_buf_line_count(args.buf)
+        if mark[1] > 0 and mark[1] <= line_count then
+            vim.cmd('normal! g`"zz')
+        end
+    end,
+})
+
+-- Show cursorline only on active windows
+autocmd({ "InsertLeave", "WinEnter" }, {
+    callback = function()
+        if vim.w.auto_cursorline then
+            vim.wo.cursorline = true
+            vim.w.auto_cursorline = false
+        end
+    end,
+})
+
+autocmd({ "InsertEnter", "WinLeave" }, {
+    callback = function()
+        if vim.wo.cursorline then
+            vim.w.auto_cursorline = true
+            vim.wo.cursorline = false
+        end
+    end,
 })
 
 --          ╭─────────────────────────────────────────────────────────╮
