e76d58c3d56cd0f8274936be13f8f2c0e66aa740
Nuclear Squid <leo@cazenave.cc>
2026-01-29 12:56:21 +0100

    fixed indentation

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -4,48 +4,48 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+    imports =
+        [ (modulesPath + "/installer/scan/not-detected.nix")
+        ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
+    boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ ];
+    boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/9a05b075-bdbd-4c5f-915a-2453b6d0b29d";
-      fsType = "ext4";
-    };
+    fileSystems."/" =
+        { device = "/dev/disk/by-uuid/9a05b075-bdbd-4c5f-915a-2453b6d0b29d";
+            fsType = "ext4";
+        };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/2DEA-E195";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
+    fileSystems."/boot" =
+        { device = "/dev/disk/by-uuid/2DEA-E195";
+            fsType = "vfat";
+            options = [ "fmask=0022" "dmask=0022" ];
+        };
 
-  swapDevices = [ ];
+    swapDevices = [ ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
+    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+    # (the default) this is the recommended approach. When using systemd-networkd it's
+    # still possible to use this option, but it's recommended to use it in conjunction
+    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+    networking.useDHCP = lib.mkDefault true;
+    # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+    # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  # hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    # hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  # hardware.pulseaudio.enable = true;
+    # hardware.pulseaudio.enable = true;
 
-  hardware.keyboard.qmk.enable = true;
+    hardware.keyboard.qmk.enable = true;
 
-  # hardware.graphics = {
-  #   enable = true;
-  #   extraPackages = with pkgs; [ intel-media-sdk intel-media-driver intel-ocl intel-vaapi-driver ];
-  #   # extraPackages = with pkgs; [ vpl-gpu-rt ];
-  # };
+    # hardware.graphics = {
+    #   enable = true;
+    #   extraPackages = with pkgs; [ intel-media-sdk intel-media-driver intel-ocl intel-vaapi-driver ];
+    #   # extraPackages = with pkgs; [ vpl-gpu-rt ];
+    # };
 
-  # hardware.intelgpu.vaapiDriver = "intel-media-driver";
+    # hardware.intelgpu.vaapiDriver = "intel-media-driver";
 }
