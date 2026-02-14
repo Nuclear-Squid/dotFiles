d9b83b8d9237b677306074a63cfc0c8e6a3b09d9
Nuclear Squid <leo@cazenave.cc>
2026-02-14 18:19:28 +0100

    Nvim: Ltex-ls

--- nvim/lsp/ltexls.lua
+++ nvim/lsp/ltexls.lua
@@ -50,9 +50,11 @@ end
 return {
   cmd = { 'ltex-ls' },
   filetypes = filetypes,
-  root_dir = function(fname)
-    return vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
-  end,
+  root_dir = '/home/nuclear-squid/Code/Snorbi/sciduino_rapport/doc/polytech.grenoble-inp.fr/',
+  -- root_dir = function(fname)
+  --   print(fname)
+  --   return vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
+  -- end,
   single_file_support = true,
   get_language_id = get_language_id,
   settings = {
--- nvim/lsp/ltexls_plus.lua
+++ nvim/lsp/ltexls_plus.lua
@@ -34,9 +34,10 @@ return {
     'typst',
     'xhtml',
   },
-  root_dir = function(fname)
-    return vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
-  end,
+  root_dir = '/home/nuclear-squid/Code/Snorbi/sciduino_rapport/doc/polytech.grenoble-inp.fr/',
+  -- root_dir = function(fname)
+  --   return vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
+  -- end,
   single_file_support = true,
   get_language_id = get_language_id,
   settings = {
