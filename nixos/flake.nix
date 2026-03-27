75405b54ca7caab7a9db7c6840e339552c230ae7
Nuclear Squid <leo@cazenave.cc>
2026-03-27 14:52:38 +0100

    Home manager as flake & nixos module.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url    = "github:nixos/nixpkgs/nixos-25.11";
     unstable.url   = "github:nixos/nixpkgs/nixos-unstable";
     old-stable.url = "github:nixos/nixpkgs/nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     zen-browser = {
@@ -20,7 +20,9 @@
     # };
   };
 
-  outputs = { nixpkgs, ... } @ inputs: {
+  outputs = { nixpkgs, home-manager, ... } @ inputs:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
@@ -31,7 +33,24 @@
             inputs.prismlauncher.packages.${pkgs.stdenv.hostPlatform.system}.prismlauncher
           ];
         })
+
+        home-manager.nixosModules.default
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.nuclear-squid = ./home.nix;
+            extraSpecialArgs = { inherit inputs; };
+            backupFileExtension = ".hm_backup";
+          };
+        }
       ];
+    };
+
+    homeConfigurations.nuclear-squid = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      extraSpecialArgs = { inherit inputs; };
+      modules = [ ./home.nix ];
     };
 
     templates.basic_shell = {
