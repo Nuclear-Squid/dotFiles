24ea7a8b87125dfc4dabba9ac62530abb0865450
Nuclear Squid <leo@cazenave.cc>
2026-06-07 11:11:14 +0200

    NixOS: Upgrade to 26.05

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,9 +1,9 @@
 {
   description = "My laptop system confituration";
   inputs = {
-    nixpkgs.url    = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url    = "github:nixos/nixpkgs/nixos-26.05";
     unstable.url   = "github:nixos/nixpkgs/nixos-unstable";
-    old-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    old-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
