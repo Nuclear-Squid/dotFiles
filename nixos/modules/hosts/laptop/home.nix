bff27b211fa35b794273eac22997f0c1436f84c8
Nuclear Squid <leo@cazenave.cc>
2026-06-25 21:54:38 +0200

    cleaner hm module

--- nixos/modules/hosts/laptop/home.nix
+++ nixos/modules/hosts/laptop/home.nix
@@ -4,23 +4,22 @@
   # with the home-manager command
   flake.homeConfigurations.nucelar-squid = inputs.home-manager.lib.homeManagerConfiguration {
     pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
-    modules = [
-      self.homeModules.nuclear-squid
-      {
-        home.username = "nuclear-squid";
-        home.homeDirectory = "/home/nuclear-squid";
-      }
-    ];
+    modules = [ self.homeModules.nuclear-squid ];
   };
 
   # This is your home.nix, your module where you configure home-manager
   # It's imported both in standalone configuration above, and in your nixos configuration
-  flake.homeModules.nuclear-squid = { pkgs, ... }: {
+  flake.homeModules.nuclear-squid = { pkgs, ... }:
+  let homeDir = "/home/nuclear-squid";
+  in {
     programs.bash.enable = true;
     programs.bash.shellAliases.ll = "ls -l";
 
-    home.packages = [ pkgs.hello ];
-    home.stateVersion = "24.11";
+    home = {
+      username = "nuclear-squid";
+      homeDirectory = homeDir;
+      packages = [ pkgs.hello ];
+      stateVersion = "24.11";
+    };
   };
-
 }
