9b0b78fad365b9969431c5ed9cb6e07bfa37887c
Nuclear Squid <leo@cazenave.cc>
2026-06-26 15:09:09 +0200

    backported my old os config

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,12 +4,25 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    import-tree.url = "github:vic/import-tree";
+    old-stable.url = "github:nixos/nixpkgs/nixos-25.11";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "unstable";
+    };
+
+    prismlauncher = {
+      url = "github:PrismLauncher/PrismLauncher";
+      inputs.nixpkgs.follows = "unstable";
+    };
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules) // {
--- nixos/modules/hosts/laptop/configuration.nix
+++ nixos/modules/hosts/laptop/configuration.nix
@@ -9,21 +9,404 @@
     ];
   };
 
+  # # This is your configuration.nix, a place where you configure your system
+  # # You can place it in a separate file.
+  # flake.nixosModules.laptopModule = { pkgs, ... }: {
+  #   environment.systemPackages = [
+  #     pkgs.vim
+  #     pkgs.firefox
+  #   ];
+  #
+  #   programs.fish.enable = true;
+  #
+  #   users.users.nuclear-squid = {
+  #     isNormalUser = true;
+  #     shell = pkgs.fish;
+  #   };
+  #   home-manager.users.nuclear-squid = self.homeModules.nuclear-squid;
+  # };
+
   # This is your configuration.nix, a place where you configure your system
   # You can place it in a separate file.
-  flake.nixosModules.laptopModule = { pkgs, ... }: {
-    environment.systemPackages = [
-      pkgs.vim
-      pkgs.firefox
-    ];
+  flake.nixosModules.laptopModule = { pkgs, config, ... }: let
+    unstable   = import inputs.unstable { system = pkgs.stdenv.hostPlatform.system; config.allowUnfree = true; };
+    old-stable = inputs.old-stable.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+    customI3   = true;
+    dotFilesRoot = ../../../..;
+  in let global-system-packages = with pkgs; {
+      code-editors = [
+        unstable.neovim
+        unstable.lazygit
+      ];
 
-    programs.fish.enable = true;
+      lsp-servers = [
+        unstable.arduino-language-server
+        clang-tools
+        languagetool
+        ltex-ls-plus
+        ltex-ls
+        ty
+      ];
 
-    users.users.nuclear-squid = {
-      isNormalUser = true;
-      shell = pkgs.fish;
+      dev-tools = [
+        valgrind
+        gnumake
+        cmake
+        serie
+        act  # Build GitHub’s CI locally
+        # those should go in Ergo‑L’s repo
+        unstable.hugo
+        unstable.pandoc
+        nginx
+      ];
+
+      languages-and-compilers = [
+        unstable.cargo
+        scilab-bin
+        python3
+        clang
+      ];
+
+      rice-and-cli-tools = [
+        xwayland-satellite # xwayland support
+        old-stable.llpp
+        onefetch
+        tealdeer
+        ripgrep
+        feh
+        fd
+        nh
+      ];
+
+      gui-apps = [
+        inputs.zen-browser.packages.x86_64-linux.default
+        simplescreenrecorder
+        libreoffice-qt
+        firefox
+        tor-browser
+        thunderbird
+        thunar
+        pcmanfm
+        element-desktop
+        picoscope
+        hunspell # Libs for libreoffice
+        hunspellDicts.uk_UA
+        hunspellDicts.th_TH
+      ];
+
+      chat = [
+        spotify
+        discord
+        telegram-desktop
+        signal-desktop
+        zulip
+        zulip-term
+      ];
+
+      art-apps = [
+        kdePackages.kdenlive
+        old-stable.ardour  # Stable and Unstable versions are broken.
+        # unstable.musescore
+        # unstable.muse-sounds-manager
+        inkscape
+        unstable.blender
+        unstable.krita
+        unstable.gimp
+      ];
+
+      keyboard-stuff = [
+        unstable.kanata
+        unstable.qmk
+        unstable.chrysalis
+      ];
+
+      lower-level-system = [
+        brightnessctl
+        xmodmap
+        pulseaudio
+        # signaldctl
+        alsa-lib
+        iptables # needed by waydroid
+        libiconv
+        xdotool
+        killall
+        bottom
+        btop
+        xclip
+        wl-clipboard-rs
+        unzip
+        wget
+        curl
+        zip
+      ];
+
+      linked-libraries = [
+        libxcursor # needed by Niri for some reason ?
+        libxi
+      ];
+
+      miscellaneous = [
+        protontricks  # For Steam proton
+        home-manager
+        xfce4-screenshooter
+        # love  # 2d lua game engine, for olympus (celeste mod installer)
+        unstable.olympus  # Celeste mod installer
+        jay  # Wayland compositor I wanna try out
+      ];
+
     };
-    home-manager.users.nuclear-squid = self.homeModules.nuclear-squid;
+  in {
+    # Allow unfree packages
+    nixpkgs.config = {
+        allowUnfree = true;
+        pulseaudio = true;
+
+        permittedInsecurePackages = [
+            "electron-39.8.10"  # Needed by Zulip, see https://github.com/NixOS/nixpkgs/pull/526892
+        ];
+    };
+
+    nix = {
+        optimise.automatic = true; # Optimise storage space of NixOS
+
+        settings = {
+            experimental-features = [ "nix-command" "flakes" ];
+            warn-dirty = false; # Please stop yelling at me everytime I run `nix develop`
+        };
+
+        gc = {
+            automatic = true;
+            dates = "weekly";
+            options = "--delete-older-than 30d";
+        };
+    };
+
+    networking.hostName = "nixos"; # Define your hostname.
+    networking.nftables.enable = true; # Needed by waydroid
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # Set your time zone.
+    time.timeZone = "Europe/Paris";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_GB.UTF-8";
+
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "fr_FR.UTF-8";
+        LC_IDENTIFICATION = "fr_FR.UTF-8";
+        LC_MEASUREMENT = "fr_FR.UTF-8";
+        LC_MONETARY = "fr_FR.UTF-8";
+        LC_NAME = "fr_FR.UTF-8";
+        LC_NUMERIC = "fr_FR.UTF-8";
+        LC_PAPER = "fr_FR.UTF-8";
+        LC_TELEPHONE = "fr_FR.UTF-8";
+        LC_TIME = "fr_FR.UTF-8";
+    };
+
+    users.defaultUserShell = pkgs.fish;
+    users.users.nuclear-squid = {
+        isNormalUser = true;
+        description = "Nuclear Squid";
+        extraGroups = [
+            "networkmanager"
+            "wheel"  # Enable 'sudo' for the user.
+            "audio"
+            "dialout"  # Allow access to serial device (for Arduino dev)
+            "docker"  # Allow using docker without root access
+            "nginx"  # Allow using nginx in localhost
+        ];
+        packages = with pkgs; [];
+    };
+
+    specialisation = {
+        powersave.configuration = {
+            services.tlp = {
+                enable = true;
+                settings = {
+                    CPU_SCALING_GOVERNOR_ON_AC  = "performance";
+                    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+                    PLATFORM_PROFILE_ON_BAT     = "low-power";
+
+                    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+                    CPU_ENERGY_PERF_POLICY_ON_AC  = "performance";
+                    PLATFORM_PROFILE_ON_AC        = "performance";
+
+                    CPU_MIN_PERF_ON_AC  = 0;
+                    CPU_MAX_PERF_ON_AC  = 100;
+                    CPU_MIN_PERF_ON_BAT = 0;
+                    CPU_MAX_PERF_ON_BAT = 20;
+
+                   # Helps save long term battery health
+                   START_CHARGE_THRESH_BAT0 = 40;  # 40 and bellow it starts to charge
+                   STOP_CHARGE_THRESH_BAT0 = 80;   # 80 and above it stops charging
+                };
+            };
+        };
+    };
+
+    services = {
+        # Auto maunt usb devices
+        udisks2.enable = true;
+        devmon.enable  = true;
+        gvfs.enable    = true;
+
+        pulseaudio.enable = true;
+        pipewire.enable   = false;
+
+        flatpak.enable = true;
+
+        upower.enable = true; # needed by the battery widget of noctalia
+
+        xserver = {
+            enable = true;
+            xkb.layout  = "fr";
+            xkb.variant = "ergol";
+            # xkb.options = "compose:102";
+
+            # videoDrivers = [ "intel" ];
+            desktopManager.xterm.enable = false;
+
+            windowManager.i3 = {
+                enable = true;
+                package =
+                    if customI3
+                    then unstable.i3.overrideAttrs {
+                        patches = [ (dotFilesRoot + /i3/0001-Added-option-to-hide-title-bar-on-tabs-and-staks.patch) ];
+                        doCheck = false;
+                    }
+                    else unstable.i3-rounded;
+            };
+
+            displayManager.sessionCommands = ''
+                ${pkgs.xmodmap}/bin/xmodmap -e "remove mod3 = Hyper_L"
+                ${pkgs.xmodmap}/bin/xmodmap -e "add mod4 = Hyper_L"
+            '';
+        };
+
+        thermald.enable = true;
+
+        kanata = {
+            enable = true;
+            package = unstable.kanata;
+            keyboards.laptop = {
+                devices = [ "/dev/input/event0" ];
+                config = builtins.readFile (dotFilesRoot + /kanata.kbd);
+                extraDefCfg = ''
+                    sequence-input-mode hidden-delay-type
+                    process-unmapped-keys yes
+                    concurrent-tap-hold yes
+                    chords-v2-min-idle 120
+                '';
+            };
+        };
+
+        nginx = {
+            enable = true;
+            recommendedGzipSettings  = true;
+            recommendedOptimisation  = true;
+            recommendedProxySettings = true;
+            recommendedTlsSettings   = true;
+
+            virtualHosts.localhost = {
+                # addSSL = true;
+                # enableACME = true;
+                # default = true;
+                root = "${config.users.users.nuclear-squid.home}/Code/www/";
+                # locations."/var/html/".proxyPass = "http://localhost:8000";
+            };
+            # appendHttpConfig = "listen 127.0.0.1:80";
+        };
+
+        # Upload programs to Mbed arduino boards
+        udev.extraRules = ''
+            SUBSYSTEMS=="usb", ATTRS{idVendor}=="2e8a", MODE:="0666"
+            SUBSYSTEMS=="usb", ATTRS{idVendor}=="2341", MODE:="0666"
+            SUBSYSTEMS=="usb", ATTRS{idVendor}=="1fc9", MODE:="0666"
+            SUBSYSTEMS=="usb", ATTRS{idVendor}=="0525", MODE:="0666"
+        '';
+
+        # udev.packages = with pkgs; [ via ];
+    };
+
+    programs = {
+        nix-ld.enable = true;
+        fish.enable = true;
+        # ssh.startAgent = true;
+
+        steam = {
+            enable = true;
+            package = unstable.steam;
+            # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+            # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+        };
+
+        niri = {
+            enable = true;
+            package = unstable.niri;
+        };
+    };
+
+    # Docker
+    virtualisation.docker.rootless = {
+        enable = true;
+        setSocketVariable = true;
+    };
+
+    virtualisation.waydroid.enable = true;
+
+    environment = {
+        pathsToLink = [
+            "/libexec"    # Needed by i3
+            "/share/zsh"  # Needed by zsh
+        ];
+
+        variables = {
+            EDITOR = "nvim";
+            EXA_COLORS = "di=01;35:uu=03;33:ur=33:uw=33:gw=33:gx=01;32:tw=33:tx=01;32:sn=35";
+            LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath global-system-packages.linked-libraries}:$LD_LIBRARY_PATH";
+        };
+
+        systemPackages = builtins.concatLists (builtins.attrValues global-system-packages);
+    };
+
+    xdg.portal = {
+        enable = true;
+        extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+        configPackages = [ pkgs.niri ];
+    };
+
+    fonts.packages = with pkgs.nerd-fonts; [
+        fantasque-sans-mono
+        monaspace
+    ];
+    # fonts.packages = [ pkgs.nerdfonts ];
+    # fonts.fontconfig.useEmbeddedBitmaps = true;
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
+
+    # List services that you want to enable:
+
+    # Enable the OpenSSH daemon.
+    # services.openssh.enable = true;
+
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
   };
 
   flake.nixosModules.laptopHardware = { config, lib, pkgs, modulesPath, ... }: {
