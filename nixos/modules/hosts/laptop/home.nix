eb15b2b9b1069dab34444890882decd4d869e912
Nuclear Squid <leo@cazenave.cc>
2026-06-26 14:53:15 +0200

    backported my home-manager config

--- nixos/modules/hosts/laptop/home.nix
+++ nixos/modules/hosts/laptop/home.nix
@@ -3,23 +3,240 @@
   # This is your standalone home-manager configuration, meant to be used on non-nixos machines
   # with the home-manager command
   flake.homeConfigurations.nucelar-squid = inputs.home-manager.lib.homeManagerConfiguration {
-    pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+    pkgs = import inputs.nixpkgs {
+      system = "x86_64-linux";
+      # config.permittedInsecurePackages = [
+      #   "librewolf-151.0.2-1"
+      # ];
+    };
     modules = [ self.homeModules.nuclear-squid ];
+    extraSpecialArgs = { inherit inputs; };
+    pecialArgs = { inherit inputs; };
   };
 
   # This is your home.nix, your module where you configure home-manager
   # It's imported both in standalone configuration above, and in your nixos configuration
-  flake.homeModules.nuclear-squid = { pkgs, ... }:
-  let homeDir = "/home/nuclear-squid";
+  flake.homeModules.nuclear-squid = { pkgs, lib, ... }: let
+    unstable = inputs.unstable.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+    homeDir = "/home/nuclear-squid";
+    dotFilesRoot = ../../../..;
   in {
-    programs.bash.enable = true;
-    programs.bash.shellAliases.ll = "ls -l";
+    # overlays = {
+    #   nixpkgsUnstable = _: prev: {
+    #     unstable = import inputs.unstable {
+    #       inherit (prev.pkgs.stdenv.hostPlatform) system;
+    #     };
+    #   };
+    # };
 
     home = {
       username = "nuclear-squid";
       homeDirectory = homeDir;
       packages = [ pkgs.hello ];
       stateVersion = "24.11";
+    };
+
+    # nixpkgs.config.permittedInsecurePackages = [
+    #   "librewolf-151.0.2-1"
+    #   "librewolf-unwrapped-151.0.2-1"
+    # ];
+
+    xdg = {
+      configHome = "${homeDir}/.config";
+      enable = true;
+    };
+
+    gtk = {
+      enable = true;
+      colorScheme = "dark";
+      theme = {
+        name = "Kanagawa-B";
+        package = pkgs.kanagawa-gtk-theme;
+      };
+    };
+
+    services.polkit-gnome.enable = true; # polkit
+
+    # programs.waybar.enable = true;
+
+    programs.neovide = {
+      enable = true;
+      package = unstable.neovide;
+      settings.backtraces_path = "$HOME/.local/share/neovide";
+      settings.font = {
+        size = 16;
+        normal        = { family = "FantasqueSansM Nerd Font Mono"; style = "regular"; };
+        bold          = { family = "FantasqueSansM Nerd Font Mono"; style = "bold"; };
+        italic        = { family = "MonaspiceRn Nerd Font Mono";    style = "regular"; };
+        bold_italic   = { family = "MonaspiceRn Nerd Font Mono";    style = "italic"; };
+      };
+    };
+
+    programs.git = {
+      enable = true;
+      lfs.enable = true;
+
+      settings = {
+        push.autoSetupRemote = true;
+
+        user = {
+          name = "Nuclear-Squid";
+          email = "leo@cazenave.cc";
+        };
+
+        alias = {
+          cv = "commit -v";
+          cb = "checkout -b";
+          st = "status";
+          lo = "log --graph --oneline";
+          pf = "push --force-with-lease";
+        };
+      };
+    };
+
+    programs.delta = {
+      enable = true;
+      enableGitIntegration = true;
+    };
+
+    programs.gh = {
+      enable = true;
+      extensions = [ pkgs.gh-notify ];
+    };
+
+    programs.kitty = {
+      enable = true;
+      package = unstable.kitty;
+      font = {
+        name = "FantasqueSansM Nerd Font Mono";
+        # name = "Operator-caska";
+        size = 10;
+      };
+      settings = {
+        bell_path = "~/Code/dotFiles/Windows_XP_Error_sound_effect.wav";
+        cursor = "#666666";
+        background_opacity = "0.8";
+        foreground = "#f7dec7";
+        background = "#1a0c24";
+        color0  = "#373354";
+        color1  = "#c02030";
+        color2  = "#3bb846";
+        color3  = "#dd9046";
+        color4  = "#2a68c8";
+        color5  = "#b02cc0";
+        color6  = "#48d5aa";
+        color7  = "#9C9BBA";
+        color8  = "#4b4673";
+        color9  = "#e86671";
+        color10 = "#8ebd6b";
+        color11 = "#e5c07b";
+        color12 = "#5ab0f6";
+        color13 = "#c678dd";
+        color14 = "#48d5aa";
+        color15 = "#f5d9de";
+        transparent_background_colors = "#1b1127@0.85 #231e36@0.85 #2d2a45@0.85 #373354@0.85";
+      };
+      keybindings = {
+        "ctrl+c" = "copy_or_interrupt";
+        "ctrl+backspace" = "send_text all \\x17";     # ctrl  + backspace
+        "shift+enter" = "send_text all \\x1b[13;2u";  # shift + enter
+      };
+    };
+
+    programs.zoxide = {
+      enable = true;
+      options = [ "--cmd" "t" ];
+    };
+
+    programs.fish = {
+      enable = true;
+      shellInit = builtins.readFile (dotFilesRoot + /shell/fish_config.fish);
+      interactiveShellInit = ''
+            functions --copy t zoxide_wrapper
+            function t --wraps=t
+                zoxide_wrapper $argv
+                git_repo_changed && clear && onefetch
+                magic_ls
+                nix_flake_available && nix develop
+            end
+
+            functions --copy ti zoxide_interactive_wrapper
+            function ti --wraps=ti
+                zoxide_interactive_wrapper $argv
+                git_repo_changed && clear && onefetch
+                magic_ls
+                nix_flake_available && nix develop
+            end
+      '';
+    };
+
+    programs.starship = {
+      enable = true;
+      settings = {
+        character = {
+          success_symbol = "[|>](bold green)";
+          error_symbol   = "[!!](bold red)";
+        };
+
+        directory.truncation_length = 5;
+      };
+    };
+
+    programs.fastfetch.enable = true;
+
+    programs.eza = {
+      enable = true;
+      enableFishIntegration = true;
+      git = true;
+      icons = "auto";
+      extraOptions = [
+        "--group-directories-first"
+        "--git"
+        "--icons"
+        "--no-quotes"
+      ] ;
+    };
+
+    programs.bat.enable = true;
+    programs.yazi.enable = true;
+
+    programs.fzf = rec {
+      enable = true;
+      package = unstable.fzf;
+      enableFishIntegration = true;
+      defaultCommand = "fd --type f --strip-cwd-prefix";
+
+      # ctrl-t
+      fileWidgetCommand = "fd --type f --type d --strip-cwd-prefix";
+      fileWidgetOptions = [
+        "--preview 'bat --color=always --style=plain -r :200 {}'"
+      ];
+    };
+
+    programs.rofi = {
+      enable = true;
+      theme = dotFilesRoot + /rofi_theme.rasi;
+    };
+
+    programs.librewolf = {
+      enable = true;
+      # package = unstable.librewolf;
+      settings = {
+        "webgl.disabled" = false;
+        "identity.fxaccounts.enable" = true;
+        "privacy.resistFingerprinting" = true;
+        "privacy.clearOnShutdown.history" = true;
+        "privacy.clearOnShutdown.cookies" = true;
+      };
+    };
+
+    programs.cava.enable = true;
+
+    services.polybar = {
+      enable = true;
+      config = dotFilesRoot + /polybar/config.ini;
+      package = pkgs.polybar.override { i3Support = true; };
+      script = "";
     };
   };
 }
