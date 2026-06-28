6830a59494a40602bed0a62477b9bca2dd9b0b13
Nuclear Squid <leo@cazenave.cc>
2026-06-28 16:50:03 +0200

    moving out flake-parts config to it’s own fucking file

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -25,17 +25,5 @@
     import-tree.url = "github:vic/import-tree";
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules) // {
-    imports = [
-      # adds home-manager options to flake-parts
-      inputs.home-manager.flakeModules.home-manager
-    ];
-
-    config.systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-  };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
 }
