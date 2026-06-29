5bea4ea28ee0f4cb01a612603501242a33d0824a
Nuclear Squid <leo@cazenave.cc>
2026-06-29 18:36:58 +0200

    wip wrappers

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -23,6 +23,11 @@
 
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
+
+    wrapper-modules = {
+      url = "github:BirdeeHub/nix-wrapper-modules";
+      inputs.nixpkgs.follows = "unstable";
+    };
   };
 
   outputs = inputs:
